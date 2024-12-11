6e32330476e6895cb89951d9a99b796857453a87
tom832 <1600011759@pku.edu.cn>
2024-12-11 20:02:27 +0800

    add download scripts

---
+++ download_data_and_model.sh
@@ -0,0 +1,6 @@
+wget -O data.tar.gz "https://zenodo.org/records/14383454/files/data.tar.gz?download=1"
+tar -xzf data.tar.gz
+rm data.tar.gz
+wget -O AutogluonModels.tar.gz "https://zenodo.org/records/14384890/files/AutoglounModels.tar.gz?download=1"
+tar -xzf AutogluonModels.tar.gz
+rm AutogluonModels.tar.gz
