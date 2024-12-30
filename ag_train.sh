7e9cebd03c8b94ac4caf56a9c8ec77a3736db5f5
tom832 <1600011759@pku.edu.cn>
2024-12-31 01:49:52 +0800

    update

--- ag_train.sh
+++ ag_train.sh
@@ -3,42 +3,42 @@ python ag_train.py \
     --class_num 5 \
     --feat tsfresh_raw \
     --ag_train_quality best_quality \
-    --hours 1.0 \
+    --hours 4.0 \
     --num_cpus 10 \
     --num_gpus 1 \
     --verbose 0 \
     --evaluate_on_test_data
 
-python ag_train.py \
-    --sp_mode s \
-    --class_num 5 \
-    --feat tsfresh_raw \
-    --ag_train_quality best_quality \
-    --hours 2.0 \
-    --num_cpus 10 \
-    --num_gpus 1 \
-    --verbose 0 \
-    --evaluate_on_test_data
+# python ag_train.py \
+#     --sp_mode s \
+#     --class_num 5 \
+#     --feat tsfresh_raw \
+#     --ag_train_quality best_quality \
+#     --hours 2.0 \
+#     --num_cpus 10 \
+#     --num_gpus 1 \
+#     --verbose 0 \
+#     --evaluate_on_test_data
 
-
-python ag_train.py \
-    --sp_mode s \
-    --class_num 6 \
-    --feat tsfresh_raw \
-    --ag_train_quality best_quality \
-    --hours 1.0 \
-    --num_cpus 10 \
-    --num_gpus 1 \
-    --verbose 0 \
-    --evaluate_on_test_data
 
 python ag_train.py \
     --sp_mode s \
     --class_num 6 \
     --feat tsfresh_raw \
     --ag_train_quality best_quality \
-    --hours 2.0 \
+    --hours 4.0 \
     --num_cpus 10 \
     --num_gpus 1 \
     --verbose 0 \
     --evaluate_on_test_data
+
+# python ag_train.py \
+#     --sp_mode s \
+#     --class_num 6 \
+#     --feat tsfresh_raw \
+#     --ag_train_quality best_quality \
+#     --hours 2.0 \
+#     --num_cpus 10 \
+#     --num_gpus 1 \
+#     --verbose 0 \
+#     --evaluate_on_test_data
