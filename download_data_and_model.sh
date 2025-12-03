5102d1f882c139ee8241b7128befdcf05e74b228
tom832 <1600011759@pku.edu.cn>
2025-12-03 16:04:13 +0800

    1) update model and dataset zenodo url; 2) add feature importance analysis for ML model

--- download_data_and_model.sh
+++ download_data_and_model.sh
@@ -1,6 +1,6 @@
-wget -O data.tar.gz "https://zenodo.org/records/16778280/files/data.tar.gz?download=1"
+wget -O data.tar.gz "https://zenodo.org/records/17773482/files/data.tar.gz?download=1"
 tar -xzf data.tar.gz
 rm data.tar.gz
-wget -O AutogluonModels.tar.gz "https://zenodo.org/records/16778359/files/AutoglounModels.tar.gz?download=1"
+wget -O AutogluonModels.tar.gz "https://zenodo.org/records/17773473/files/AutoglounModels.tar.gz?download=1"
 tar -xzf AutogluonModels.tar.gz
 rm AutogluonModels.tar.gz
